1a4587fe2ece0cb97e0ccc7c95251a7f903f6637
DeadRick <vndemyanenko@edu.hse.ru>
2022-12-10 01:08:15 +0300

    2nd task
    Задание 2. Создать новый проект, написать программу, которая выводит в элемент TextView надпись, введённую пользователем в текстовом поле EditText после нажатия на кнопку Button. Запустить на эмуляторе и убедиться, что всё работает.
    
    Added button with some functionality

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button buttonText = findViewById(R.id.showTextButton);
+        EditText textToEnter = findViewById(R.id.textToShow);
+        TextView enteredText = findViewById(R.id.textEntered);
+        TextView clickedText = findViewById(R.id.showClick);
+        final Integer[] clickCount = {1};
+
+        buttonText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickedText.setText("Click count: " + clickCount[0]);
+                if (textToEnter.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Text is empty!", Toast.LENGTH_SHORT).show();
+                } else {
+                    enteredText.setText(textToEnter.getText().toString());
+                }
+                clickCount[0] += 1;
+            }
+        });
+
     }
 }
